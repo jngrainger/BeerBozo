a087a7189e360fd16295c22277f27e9eae86c9e1
unknown <jngrainger@gmail.com>
2014-08-06 00:04:00 +0100

    the list

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -2,65 +2,39 @@
 
 angular.module('beerBozo')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+    $scope.topBeers = [
       {
-        'key': 'angular',
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        key: 'Kernel',
+        title: 'Kernel Pale',
+        url: 'https://angularjs.org/',
+        description: 'Pale Ale, Tasty Shit',
+        logo: 'Kernel.jpg',
+        rank: 4
       },
       {
-        'key': 'browsersync',
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
+        key: 'Goose Island',
+        title: 'Goose Island IPA',
+        url: 'http://browsersync.io/',
+        description: 'Punch in the Mouth',
+        logo: 'gooseisland.jpg',
+        rank: 5
       },
       {
-        'key': 'gulp',
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
+        key: 'Magic Rock',
+        title: 'Magic Rock High Wire',
+        url: 'http://gulpjs.com/',
+        description: 'Yum.',
+        logo: 'magicRock.jpg',
+        rank: 4
       },
       {
-        'key': 'jasmine',
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'key': 'karma',
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'key': 'protractor',
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'key': 'jquery',
-        'title': 'jQuery',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'jquery.jpg'
-      },
-      {
-        'key': 'foundation',
-        'title': 'Foundation',
-        'url': 'http://foundation.zurb.com/',
-        'description': 'The most advanced responsive front-end framework in the world.',
-        'logo': 'foundation.png'
-      }
+        key: 'Beaver',
+        title: 'BeaverTown Smog Rocket',
+        url: 'http://jasmine.github.io/',
+        description: 'Behavior-Driven JavaScript.',
+        logo: 'smog.jpg',
+        rank: 4
+      }      
     ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+    
   });
